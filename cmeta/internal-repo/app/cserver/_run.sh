419aba9f4be89fbec1d2f4822ec611ef8ce7643c
Grigori Fursin <gfursin@gmail.com>
2025-12-16 16:37:19 +0100

    clean up

--- cmeta/internal-repo/app/cserver/_run.sh
+++ cmeta/internal-repo/app/cserver/_run.sh
@@ -1,4 +1,4 @@
-@echo off
+#!/bin/bash
 
 ${PYTHON_PREFIX} python -m pip install -r requirements.txt
 ${PYTHON_PREFIX} python -m uvicorn src.app:app --host ${CSERVER_HOST} --port ${CSERVER_PORT} --reload
